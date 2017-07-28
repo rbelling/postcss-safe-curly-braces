4dca711a9fd531b7e99ba9a30d4b9b08234b70e7
Riccardo Bellingeri <rbellingeri@gilt.com>
2017-07-28 15:35:53 +0100

    Cleanup code style, better description

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const plugin = require('postcss').plugin;
 const RULE_SEPARATOR = ' ';
 
 module.exports = plugin('postcss-safe-curly-braces', () => {
-    // Avoid back-to-back closed curly braces, that might break certain templating engines like Handlebars or Vue
-    return function (root) {
-        root.walkAtRules((atRule) => {
-            if (!atRule.raws.after.match(/^(\n|\s)/)) {
-                // If the atRule isn't preceded by a space, a tab or a newline,
-                // then prepend our `RULE_SEPARATOR` to it.
-                atRule.raws.after = `${RULE_SEPARATOR}${atRule.raws.after}`;
-            }
-        });
-    };
+  // Avoid back-to-back closed curly braces, that might break certain template engines like Handlebars or Vue
+  return function (root) {
+    root.walkAtRules((atRule) => {
+      if (!atRule.raws.after.match(/^(\n|\s)/)) {
+        // If the closing curly brace of an atRule isn't already preceded by a space, a tab or a newline,
+        // then prepend `RULE_SEPARATOR` to the curly brace.
+        atRule.raws.after = `${RULE_SEPARATOR}${atRule.raws.after}`;
+      }
+    });
+  };
 });
--- index.test.js
+++ index.test.js
@@ -1,17 +1,15 @@
 const postcss = require('postcss');
-
 const plugin = require('./');
-
-function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
-}
+const run = (input, output, opts) => {
+  return postcss([plugin(opts)]).process(input)
+    .then(result => {
+      expect(result.css).toEqual(output);
+      expect(result.warnings().length).toBe(0);
+    });
+};
 
 it('Should add a white space before the end of an AtRule, unless it is spaced already', () => {
-    const src =     '@media(hover:hover){c{color: green;}}\n@media(hover:coarse){b{color: red;}   }@media(hover:coarse){d{color: blue;}\n}@media(hover:none){f{color: teal;} }';
-    const target =  '@media(hover:hover){c{color: green;} }\n@media(hover:coarse){b{color: red;}   }@media(hover:coarse){d{color: blue;}\n}@media(hover:none){f{color: teal;} }'
-    return run(src, target, {});
+  const src = '@media(hover:hover){c{color: green;}}\n@media(hover:coarse){b{color: red;}   }@media(hover:coarse){d{color: blue;}\n}@media(hover:none){f{color: teal;} }';
+  const target = '@media(hover:hover){c{color: green;} }\n@media(hover:coarse){b{color: red;}   }@media(hover:coarse){d{color: blue;}\n}@media(hover:none){f{color: teal;} }'
+  return run(src, target, {});
 });
